89003e22405851a75773c34ea11821732ada7970
Dibandi Nguimbi Brill Joferson <EXT2023248@eni.intranet>
2026-07-05 23:56:49 +0100

    commit prod

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename={ process.env.NODE_ENV === "production" ? "/jogeek" : "/" }>
         <App />
     </BrowserRouter>
   </React.StrictMode>
